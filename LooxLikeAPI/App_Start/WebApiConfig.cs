5d5a958392b0f34036bad0e5ab97a6185ea9e416
Raffaele Lovino <raffaele.lovino.intern@yoox.com>
2015-09-04 18:40:58 +0200

    dimmi qualcosa 5.1

--- LooxLikeAPI/App_Start/WebApiConfig.cs
+++ LooxLikeAPI/App_Start/WebApiConfig.cs
@@ -17,8 +17,8 @@ namespace LooxLikeAPI
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }*/
-            );
+                defaults: new { id = RouteParameter.Optional }
+            );*/
         }
     }
 }
